3f6a7f0f01ea1fb7e32e2689d4f8a2d89d6443f1
Yerson Carhuallanqui <yerson.rc@gmail.com>
2021-05-30 20:08:05 -0500

    Footer link update

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import s from './Footer.module.css';
 
 const linkDribbble = '//dribbble.com/yerson';
-const linkMedium = '//medium.com/zurvin';
+const linkMedium = '//medium.com/hifredd';
 const linkContact = 'mailto:hola@zurvin.com';
 const linkGithubCode = '//github.com/yersoncp/zurvin-web-nextjs';
 
@@ -22,12 +22,12 @@ const Footer = () => (
             
          </div>
 
-         {/* <div>
-            <h3>Nosotros</h3>
+         <div>
+            <h4>Nosotros</h4>
             <Link href={linkMedium}><a className={s.menu} target="_blank">Blog</a></Link>
-            <Link href={linkGithubCode}><a className={s.menu} target="_blank">Open source</a></Link>
-            <Link href={linkDribbble}><a className={s.menu} target="_blank">Dribbble</a></Link>
-         </div> */}
+            {/* <Link href={linkGithubCode}><a className={s.menu} target="_blank">Open source</a></Link> */}
+            {/* <Link href={linkDribbble}><a className={s.menu} target="_blank">Dribbble</a></Link> */}
+         </div>
 
          <div>
             <h4>Contacto</h4>
